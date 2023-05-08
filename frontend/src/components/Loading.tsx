b5dc29d7a178220e58d4ee6376da50b8aa15f585
Jessy Damasceno <jessy_bas@hotmail.com>
2023-05-08 13:57:11 -0300

    fix/style: remove class h-fit in Loading component

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -5,7 +5,7 @@ import React, { FC } from 'react';
 const Loading: FC = () => {
   const lottiePath = '/loading1.gif';
   return (
-    <div className='flex justify-center align-middle h-fit'>
+    <div className='flex justify-center align-middle'>
       <Image src={lottiePath} alt="Loading" width={ 26 } height={ 26 } />
     </div>
   );
